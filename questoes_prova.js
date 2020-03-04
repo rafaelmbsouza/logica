b18819171df68f4e638df1276a06f7ac0fdebee5
rafaelmbsouza <rafaelmbsouza@gmail.com>
2020-03-04 18:30:13 -0300

    perguntas de referencia da revisao

---
+++ questoes_prova.js
@@ -0,0 +1,35 @@
+// while(negativado){
+//     console.log('O cliente está negativado. Insira o ID de outro cliente.')
+//     negativado = checaSituacao()
+// }
+
+// for(var cont=0;repeat;cont--){
+
+// }
+
+// lastNum = 5
+// ar = [0,1,2,3,4]
+// str = "rafael"
+// //ar[ar.length] = lastNum
+// //ar.push(lastNum)
+// ar = ar.concat([lastNum])
+
+// console.log(ar)
+// ar+=lastNum
+// console.log(ar)
+
+// console.log(str.splice(str.length-3))
+
+// var person = ['Rafael','luiz'];
+// person["firstName"] = "John";
+// person["lastName"] = "Doe";
+// person["age"] = 46;
+// person[5] = 'clara'
+// var x = person.length;      // person.length will return 0
+// var y = person[3];  
+
+// console.log(x)
+a = new Number(4)
+console.log(a.floor)
+
+// console.log(Math.max(...[1,2,3,4,5,3,99,1]))
